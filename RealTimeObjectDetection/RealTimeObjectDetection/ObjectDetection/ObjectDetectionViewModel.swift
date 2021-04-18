527d27a10d4d9b627fc5664a38f77fa3ef562ff6
Rodrigo Candido <rodrigoipu@hotmail.com>
2021-04-18 19:16:22 +0200

    on createMVVMProjectStructure - Create a viewModel class to handles object recognition and its delegate Implement user interface to give a feedback about what image have been detected

---
+++ RealTimeObjectDetection/RealTimeObjectDetection/ObjectDetection/ObjectDetectionViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  ObjectDetectionViewModel.swift
+//  RealTimeObjectDetection
+//
+//  Created by Rodrigo  Candido on 18/4/21.
+//
+
+import Foundation
+import Vision
+
+class ObjectDetectionViewModel{
+    var delegate: ObjectDetectionViewModelDelegate?
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MARK: - Respond to Views demands
+extension ObjectDetectionViewModel{
+    
+    func imageWasCaptured(imageBuffer: CVPixelBuffer){
+        do {
+        
+            let configuration = MLModelConfiguration()
+            let model = try VNCoreMLModel(for: YOLOv3(configuration: configuration).model)
+            
+            let request = VNCoreMLRequest(model: model) { (resultRequest, error) in
+                
+                if let _ = error {
+                    print("resquest VN error !")
+                    return
+                }
+                
+                guard let results = resultRequest.results as? [VNRecognizedObjectObservation] else { return}
+                
+                guard let observation = results.first else{
+                    self.delegate?.didNotRecognizedObject()
+                    return
+                }
+                
+                if observation.confidence > 0.9 {
+                    self.delegate?.didRecognizeObject(recognizedObjects: [observation])
+                }
+            }
+            
+            let handler = VNImageRequestHandler(cvPixelBuffer: imageBuffer, options: [:])
+            try handler.perform([request])
+            
+        } catch  {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
--- RealTimeObjectDetection/RealTimeObjectDetection/ViewController.swift
+++ RealTimeObjectDetection/RealTimeObjectDetection/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
     
     // MARK: - Class properties
-    let cameraController = CameraController()
+    private let cameraController = CameraController()
+    private var viewModel: ObjectDetectionViewModel
 
     // MARK: - Outlets
     private lazy var previewOutput: UIView = {
@@ -21,11 +23,49 @@ class ViewController: UIViewController {
         return view
     }()
     
+    private lazy var boundingView: UIView = {
+        let view = UIView()
+        let width  = self.view.bounds.width / 2
+        let height = self.view.bounds.height / 2
+        view.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        view.center = self.view.center
+        view.backgroundColor = .purple
+        view.alpha = 0.8
+        return view
+    }()
+    
+    
+    private lazy var objectLabel: UILabel = {
+        let view = UILabel()
+        view.textAlignment = .center
+        return view
+    }()
+    
+
+    // MARK: - Lifecycle
+    init(viewModel: ObjectDetectionViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: .main)
+        viewModel.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupOutlets()
         setupCameraController()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear( animated)
+        self.cameraController.stopSession()
+    }
+    
+    deinit {
+        self.cameraController.stopSession()
     }
 }
 
@@ -49,15 +89,55 @@ class ViewController: UIViewController {
 // MARK: - Extension for Outlets setup
 extension ViewController{
     private func setupOutlets(){
-        
         setupPreviewOutput()
-        
+        setupBoundingView()
+        setupObjectLabel()
     }
     
     private func setupPreviewOutput(){
         self.view.addSubview(self.previewOutput)
     }
+    
+    private func setupBoundingView(){
+        self.view.addSubview(boundingView)
+    }
+    
+    private func setupObjectLabel(){
+        self.boundingView.addSubview(objectLabel)
+        self.objectLabel.pin(to: self.boundingView)
+        self.objectLabel.text = "Reading Image..."
+    }
 }
+
+
+
+// MARK: - Dynamic UI
+extension ViewController{
+        
+    private func redrawDetectionLayers(recognizedObjects: [VNRecognizedObjectObservation]){
+        for object in recognizedObjects{
+            DispatchQueue.main.async {
+                let scaleHeight = self.previewOutput.frame.width / object.boundingBox.size.width * object.boundingBox.size.height
+                let x = object.boundingBox.midX * scaleHeight
+                let y = object.boundingBox.midY * scaleHeight
+                let width = CGFloat(100)
+                self.boundingView.frame =  CGRect(x: x, y: y, width: width, height: width)
+                self.boundingView.backgroundColor = .green
+                self.boundingView.alpha = 0.8
+                self.objectLabel.text = object.labels.first?.identifier
+                
+            }
+        }
+    }
+    
+    private func clearAlreadyDetected(){
+        DispatchQueue.main.async {
+            self.boundingView.frame = CGRect()
+        }
+    }
+    
+}
+
 
 
 
@@ -105,8 +185,53 @@ extension ViewController{
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MARK: - Extension for CameraControllerDelegate
 extension ViewController: CameraControllerDelegate{
     func didCapturedImage(imageBuffer: CVPixelBuffer, dimesions: CGSize) {
-     print("Frame")
+        self.viewModel.imageWasCaptured(imageBuffer: imageBuffer)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MARK: - Extension for ObjectDetectionViewModelDelegate
+extension ViewController: ObjectDetectionViewModelDelegate{
+    
+    func didNotRecognizedObject() {
+        self.clearAlreadyDetected()
+    }
+    
+    func didRecognizeObject(recognizedObjects: [VNRecognizedObjectObservation]) {
+       self.redrawDetectionLayers(recognizedObjects: recognizedObjects)
     }
 }
